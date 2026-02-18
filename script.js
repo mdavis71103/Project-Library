ad7eda84f9275e0478c54cc6d4b9809d0d1e0d3b
mdavis71103 <mdavis71103@gmail.com>
2026-02-18 06:46:11 -0500

    Feat: Added js for adding books and added seeds
    AddBookToLibrary was written and two seeds were created to test this and
    future functions.

---
+++ script.js
@@ -0,0 +1,17 @@
+const myLibrary = [];
+
+function Book(cover, title, description, read, id) {
+    this.cover = cover;
+    this.title = title;
+    this.description = description;
+    this.read = read;
+    this.id = id;
+}
+
+function addBookToLibrary() {
+
+}
+
+function displayBooks {
+    //Creates a card element for each book on the DOM
+}
